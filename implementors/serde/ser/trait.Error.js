218b34ad9ece323cb2f60aae49372bab1091baf3
MomoLangenstein <MomoLangenstein@users.noreply.github.com>
2021-08-31 04:03:33 +0000

    deploy: f5594cd8ce37e4bd6751dfe4bcc0d2fe63f3bd5e

--- implementors/serde/ser/trait.Error.js
+++ implementors/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ron"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.129/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"ron/error/struct.Error.html\" title=\"struct ron::error::Error\">Error</a>","synthetic":false,"types":["ron::error::Error"]}];
+implementors["ron"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.130/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"ron/error/struct.Error.html\" title=\"struct ron::error::Error\">Error</a>","synthetic":false,"types":["ron::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
