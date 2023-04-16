2f606e23c521bc337d8f99f52c5bb4e673f47570
juntyr <juntyr@users.noreply.github.com>
2023-04-16 10:32:27 +0000

    deploy: juntyr/necsim-rust@8c6e72223db7a921a44ae6d87e8a9426d564b8a4

--- implementors/serde/ser/trait.SerializeSeq.js
+++ implementors/serde/ser/trait.SerializeSeq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rustcoalescence":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.156/serde/ser/trait.SerializeSeq.html\" title=\"trait serde::ser::SerializeSeq\">SerializeSeq</a> for <a class=\"struct\" href=\"rustcoalescence/args/utils/ser/impl/struct.BufferedSequenceSerializer.html\" title=\"struct rustcoalescence::args::utils::ser::impl::BufferedSequenceSerializer\">BufferedSequenceSerializer</a>"]]
+"rustcoalescence":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.160/serde/ser/trait.SerializeSeq.html\" title=\"trait serde::ser::SerializeSeq\">SerializeSeq</a> for <a class=\"struct\" href=\"rustcoalescence/args/utils/ser/impl/struct.BufferedSequenceSerializer.html\" title=\"struct rustcoalescence::args::utils::ser::impl::BufferedSequenceSerializer\">BufferedSequenceSerializer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
