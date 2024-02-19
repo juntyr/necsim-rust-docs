c3e0f698a11982af50576526e27a49d338dc31d2
juntyr <juntyr@users.noreply.github.com>
2024-02-19 18:31:05 +0000

    deploy: juntyr/necsim-rust@cf531743d7757af8ac05d930c0c91426a2d452e2

--- trait.impl/serde/ser/trait.SerializeTuple.js
+++ trait.impl/serde/ser/trait.SerializeTuple.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rustcoalescence":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.195/serde/ser/trait.SerializeTuple.html\" title=\"trait serde::ser::SerializeTuple\">SerializeTuple</a> for <a class=\"struct\" href=\"rustcoalescence/args/utils/ser/impl/struct.BufferedTupleSerializer.html\" title=\"struct rustcoalescence::args::utils::ser::impl::BufferedTupleSerializer\">BufferedTupleSerializer</a>"]]
+"rustcoalescence":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.196/serde/ser/trait.SerializeTuple.html\" title=\"trait serde::ser::SerializeTuple\">SerializeTuple</a> for <a class=\"struct\" href=\"rustcoalescence/args/utils/ser/impl/struct.BufferedTupleSerializer.html\" title=\"struct rustcoalescence::args::utils::ser::impl::BufferedTupleSerializer\">BufferedTupleSerializer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
